f3b6d0d3300f86f2bb2bad9c4e088e4dd0ed6b53
Renato Ferreira de Souza <rfs1011@gmail.com>
2019-11-26 14:52:58 -0300

    New: Added new super class ENERGY

---
+++ src/energy.h
@@ -0,0 +1,50 @@
+#ifndef ENERGY_
+#define ENERGY_
+
+#include <string>
+
+class ENERGY
+{
+  public:
+
+
+  ENERGY(const class Simulation_Parameters * lc);
+  
+  virtual  double functional_derivative_00(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const =0;
+  virtual  double functional_derivative_01(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const =0;
+  virtual  double functional_derivative_02(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const =0;
+  virtual  double functional_derivative_11(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const =0;
+  virtual  double functional_derivative_12(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const =0;
+
+ 
+
+ protected:
+  
+  const double sigma;
+  const double a;
+  const double bb;
+  const double cc;
+  const double L1;
+  const double L2;
+  const double L3;
+  const double Lq;
+  const double Ls;
+  const double Lq_tilde;
+  const double Lambda;
+  const double Lambda_s;
+  const double p0;
+  const double q0;
+  const double  S_eq;      
+  const double deltaepslon;
+  const double elecfieldx;
+  const double elecfieldy;
+  const double elecfieldz;
+
+  //void  assert_parameter_is_set(bool parameter, class std::string parameter_name, bool has_standard_value =false);
+  
+  double dt;
+
+}
+
+
+#endif
