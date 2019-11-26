7962952a59c2b070e0ea42804e212d821be3e3ee
Renato Ferreira de Souza <rfs1011@gmail.com>
2019-11-26 15:46:01 -0300

    Refactoration: Refactored boundary class to be derived from the ENERGY class

--- src/boundary.cpp
+++ src/boundary.cpp
@@ -1,26 +1,11 @@
 #include "driver.h"
+#include "energy.h"
 #include "boundary.h"
 #include <string>
 #include <iostream>
 //base class to posterior boudary definition
-BOUNDARY::BOUNDARY(const  Simulation_Parameters *  sim_par, int boundary_number):
-  sigma(sim_par->T*sim_par->a), 
-  a(sim_par->a),
-  bb(sim_par->B),
-  cc(sim_par->C),
-  dx_1(1/sim_par->dx),
-  dy_1(1/sim_par->dy),
-  dz_1(1/sim_par->dz),
-  L1(sim_par->L1),
-  L2(sim_par->L2),
-  L3(sim_par->L3),
-  Lq(sim_par->Lq),
-  Ls(sim_par->Ls),
-  Lq_tilde(2.0*sim_par->Lq*sim_par->q0),
-  Lambda(1/sim_par->mu_1)    ,
+BOUNDARY::BOUNDARY(const  Simulation_Parameters *  sim_par, int boundary_number): ENERGY(sim_par),
   Lambda_s(1/sim_par->mu_1_s),
-  S_eq(sim_par->S_eq),
-  q0(sim_par->q0),
   boundary_id(boundary_number)
   { };
 
--- src/boundary.h
+++ src/boundary.h
@@ -3,31 +3,19 @@
 
 #include <string>
 
-class BOUNDARY
+class BOUNDARY: public ENERGY
 {
  
   
  protected:
 
   void  assert_parameter_is_set(bool parameter, std::string parameter_name, bool has_standard_value =false);
+  
 
-  const double sigma;
-  const double a;
-  const double bb;
-  const double cc;
-  const double dx_1, dy_1, dz_1;
-  const double L1;
-  const double L2;
-  const double L3;
-  const double Lq;
-  const double Ls;
-  const double Lq_tilde;
-  const double Lambda, Lambda_s;
-  const double  S_eq;
-  const double q0;
-  const double * Wo;
-  double theta_0, phi_0;
   const int boundary_id;
+  const double Lambda_s;
+  double * Wo;
+  
   std::string condition_name;
   
  public:
@@ -37,11 +25,11 @@ class BOUNDARY
 
 
 
-  virtual double surface_00(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  = 0;
-  virtual double surface_01(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  = 0;
-  virtual double surface_02(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  = 0;
-  virtual double surface_11(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  = 0;
-  virtual double surface_12(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  = 0;
+  virtual double functional_derivative_00(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const=0  ;
+  virtual double functional_derivative_01(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  =0;
+  virtual double functional_derivative_02(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  =0;
+  virtual double functional_derivative_11(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  =0;
+  virtual double functional_derivative_12(const double  QN[5],const double  dQ[15],const double  ddQ[30], const double v[3]) const  =0;
   
 };
 
